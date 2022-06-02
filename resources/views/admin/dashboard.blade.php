81b609c8d4d45c607b3fc4af9dabd479c0dbc066
trifine <trifine.laurensi08@gmail.com>
2022-06-02 14:21:07 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -46,7 +46,7 @@
                 </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-                <a href="{{ route('user.index') }}" style="text-decoration: none">
+                <a href="{{ route('userManagement.index') }}" style="text-decoration: none">
                     <div class="card border-left-info shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
